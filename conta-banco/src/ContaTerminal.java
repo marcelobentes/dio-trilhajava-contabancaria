ab47556e23dbba400639134eb546fccf28a936c7
marcelobentes <marcelo.maia.m@gmail.com>
2022-11-01 22:22:30 -0300

    informando dados de entrada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        agencia = entrada.next();
+        System.out.println("Por favor informe o número da conta: ");
+        numero = entrada.nextInt();
+        System.out.println("Por favor informe o nome do Cliente: ");
+        nomeCliente = entrada.next();
+        System.out.println("Por informar valor de deposito: ");
+        saldo = entrada.nextDouble();
+
+
     }
 }
